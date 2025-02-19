405954d72d52425910a5307a6d5bb51db61e6440
AlexanderAndersonAmenAmen <104784206+AlexanderAndersonAmenAmen@users.noreply.github.com>
2025-02-20 00:49:41 +0300

    Update stone_mask.lua

--- jokers/stone_mask.lua
+++ jokers/stone_mask.lua
@@ -2,13 +2,21 @@ SMODS.Joker {
     key = 'stone_mask',
     atlas = 'Jokers',
     pos = { x = 5, y = 1 },
-    rarity = 2,
+    rarity = 3,
     cost = 8,
     unlocked = true,
     discovered = true,
     blueprint_compat = false,
     eternal_compat = true,
     perishable_compat = true,
+    loc_vars = function(self, info_queue, card)
+		info_queue[#info_queue+1] = G.P_CENTERS.e_foil
+		info_queue[#info_queue+1] = G.P_CENTERS.e_holo
+		info_queue[#info_queue+1] = G.P_CENTERS.e_polychrome
+		return {
+			vars = { card.ability.max_highlighted }
+		}
+	end,
     calculate = function(self, card, context)
         if context.cardarea == G.play and context.individual and #context.full_hand == 1 and G.GAME.current_round.hands_played == 0 then
             if context.other_card and context.other_card.ability.set == "Enhanced" and not context.other_card.edition and not context.repetition then
@@ -24,4 +32,3 @@ SMODS.Joker {
         end
     end
 }
-
