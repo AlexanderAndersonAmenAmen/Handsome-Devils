5a233eadcfd099bdc678a5d3b0ac8f3b8954ee45
sup3p <rayraydistefano@gmail.com>
2025-02-23 19:40:15 -0500

    Seismic activity no longer spawns if you have no stones

--- jokers/seismic_activity.lua
+++ jokers/seismic_activity.lua
@@ -17,12 +17,20 @@ SMODS.Joker {
     calculate = function(self, card, context)
         if (context.cardarea == G.play or context.cardarea == G.hand) and context.repetition then
             if context.other_card.config.center == G.P_CENTERS.m_stone then
-            return {
-                message = localize('k_hnds_seismic'),
-                repetitions = card.ability.extra.repetitions,
-                card = card
-            }
+                return {
+                    message = localize('k_hnds_seismic'),
+                    repetitions = card.ability.extra.repetitions,
+                    card = card
+                }
+            end
         end
+    end,
+    in_pool = function(self, args)
+        for key, value in pairs(G.playing_cards) do
+			if value.config.center == G.P_CENTERS.m_stone then
+				return true
+			end
+		end
+        return false
     end
-end
 }
