fb0fc6e7c8f6c87df5b95e582c3ce72e84fe65bb
AlexanderAndersonAmenAmen <104784206+AlexanderAndersonAmenAmen@users.noreply.github.com>
2025-03-03 17:33:21 +0300

    dream 2.0

--- consumables/spectral/dream.lua
+++ consumables/spectral/dream.lua
@@ -2,13 +2,12 @@ SMODS.Consumable {
 	key = 'dream',
 	set = 'Spectral',
 	config = {
-        extra = {
-            dollars_per_tag = 10,
-			max_tags = 10,
-        }
-    },
+		extra = {
+			max_tags = 10
+		}
+	},
 	loc_vars = function(self, info_queue, card)
-		return {vars = {card.ability.extra.dollars_per_tag, card.ability.extra.max_tags}}
+		return { vars = { card.ability.extra.max_tags } }
 	end,
 	rarity = 4,
 	hidden = true,
@@ -16,25 +15,33 @@ SMODS.Consumable {
 	atlas = 'Consumables',
 	pos = { x = 1, y = 1 },
 	cost = 4,
-    use = function(self, card, context, copier)
+	use = function(self, card, context, copier)
 		local used_consumable = copier or card
-		local tags_to_make = math.min(math.floor(G.GAME.dollars / card.ability.extra.dollars_per_tag), card.ability.extra.max_tags)
-		ease_dollars(-G.GAME.dollars)
-		G.E_MANAGER:add_event(Event({trigger = 'before', delay = 0.75, func = function()
-			--empty event for timing purposes
-			return true end }))
+		local tags_to_make = card.ability.extra.max_tags
+		G.E_MANAGER:add_event(Event({
+			trigger = 'before',
+			delay = 0.75,
+			func = function()
+				--empty event for timing purposes
+				return true
+			end
+		}))
 		for i = 1, tags_to_make do
-			G.E_MANAGER:add_event(Event({trigger = 'before', delay = 0.5, func = function()
-				used_consumable:juice_up()
-				play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
-				add_tag(HNDS.poll_tag("dream_"..i))
-				return true end }))
+			G.E_MANAGER:add_event(Event({
+				trigger = 'before',
+				delay = 0.5,
+				func = function()
+					used_consumable:juice_up()
+					play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+					add_tag(HNDS.poll_tag("dream_",  {"tag_foil", "tag_holo", "tag_polychrome", "tag_negative", "tag_rare", "tag_uncommon","tag_buffoon"}))
+					return true
+				end
+			}))
 		end
-		
-    end,
+	end,
 	can_use = function(self, card)
 		return true
-    end,
+	end,
 	in_pool = function(self, args)
 		if G.GAME.dollars >= 30 then return true end
 		return false
