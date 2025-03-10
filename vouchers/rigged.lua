8c6f374d70c1349a4ed70298e1d3826f79d59114
AlexanderAndersonAmenAmen <104784206+AlexanderAndersonAmenAmen@users.noreply.github.com>
2025-03-10 15:55:44 +0300

    Update rigged.lua

--- vouchers/rigged.lua
+++ vouchers/rigged.lua
@@ -6,6 +6,7 @@ SMODS.Voucher({
 	cost = 10,
 	unlocked = true,
 	discovered = true,
+	requires = { "v_beginners_luck" },
 	available = true,
 	redeem = function(self)
 			G.GAME.probabilities.normal = G.GAME.probabilities.normal * 1.5
