745553bc5052cbf5ca40336487ca936948403693
Eris (borb) <remcosveen@gmail.com>
2025-09-14 22:19:43 +0200

    jackpot uses newprob and scale_card

--- jokers/jackpot.lua
+++ jokers/jackpot.lua
@@ -10,7 +10,8 @@ SMODS.Joker {
     },
     rarity = 2,
     loc_vars = function(self, info_queue, card)
-        return { vars = { ((G.GAME.probabilities.normal or 1) * card.ability.extra.prob), card.ability.extra.base_chance, card.ability.extra.money, card.ability.extra.prob_inc } }
+        local numerator, denominator = SMODS.get_probability_vars(card, card.ability.extra.prob, card.ability.extra.base_chance, "hnds_jackpot")
+        return { vars = { numerator, denominator, card.ability.extra.money, card.ability.extra.prob_inc } }
     end,
     atlas = "Jokers",
     pos = { x = 8, y = 0},
@@ -22,7 +23,7 @@ SMODS.Joker {
     perishable_compat = true,
 
     calc_dollar_bonus = function(self, card)
-        if pseudorandom("Jackpot") < (G.GAME.probabilities.normal * card.ability.extra.prob) / card.ability.extra.base_chance then
+        if SMODS.pseudorandom_probability(card, "hnds_jackpot", card.ability.extra.prob, card.ability.extra.base_chance, "hnds_jackpot") then
             SMODS.calculate_effect({message = localize("k_hnds_jackpot"), colour = G.C.MONEY }, card)
             G.E_MANAGER:add_event(Event({
                 func = function()
@@ -35,13 +36,16 @@ SMODS.Joker {
 
     calculate = function(self, card, context)
         if context.cardarea == G.play and context.individual then
-            if context.other_card:get_id() == 7 and ((G.GAME.probabilities.normal or 1) * card.ability.extra.prob) < 100 then
-                card.ability.extra.prob = card.ability.extra.prob + card.ability.extra.prob_inc
-                return {
-                    message = localize('k_hnds_probinc'),
-                    colour = G.C.GREEN,
-                    focus = card
-                }
+            if context.other_card:get_id() == 7 then
+                SMODS.scale_card(card, {
+                    ref_table = card.ability.extra,
+                    ref_value = "prob",
+                    scalar_value = "prob_inc",
+                    scaling_message = {
+                        message_key = "k_hnds_probinc",
+                        colour = G.C.GREEN
+                    }
+                })
             end
         end
     end
