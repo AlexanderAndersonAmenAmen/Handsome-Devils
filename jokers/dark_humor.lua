5e7ea8275fe1897fc2796009fae94fdaa047b8ac
Eris (borb) <remcosveen@gmail.com>
2025-09-19 22:24:13 +0200

    fix a stupid

--- jokers/dark_humor.lua
+++ jokers/dark_humor.lua
@@ -26,7 +26,7 @@ SMODS.Joker {
                 card.ability.extra.chips = card.ability.extra.chips + target:get_chip_bonus()  + target:get_chip_h_bonus()
                 card.ability.extra.mult = card.ability.extra.mult + target:get_chip_mult() + target:get_chip_h_mult()
                 SMODS.destroy_cards(target)
-                SMODS.calculate_effect({ message = localize("k_upgrade_ex") })
+                SMODS.calculate_effect({ message = localize("k_upgrade_ex") }, card)
             end
         end
         if context.joker_main or context.forcetrigger then
