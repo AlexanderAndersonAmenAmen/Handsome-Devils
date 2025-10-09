3899f5bf9a789cb23f7fbf726b052df4f641696c
Eris (borb) <remcosveen@gmail.com>
2025-10-09 19:42:19 +0200

    why so spectral

--- seals/spectralseal.lua
+++ seals/spectralseal.lua
@@ -3,21 +3,37 @@ SMODS.Seal({
     pos = { x = 2, y = 1 },
     atlas = "Extras",
     badge_colour = G.C.SECONDARY_SET.Spectral,
+    config = { extra = { cards = 2 } },
     calculate = function(self, card, context)
-        if context.destroy_card and context.cardarea == G.play or context.cardarea == G.hand and context.other_card == card and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-            G.E_MANAGER:add_event(Event({
-                trigger = 'before',
-                delay = 0.0,
-                func = function()
-                    if G.consumeables.config.card_limit > #G.consumeables.cards then
-                        SMODS.add_card({ set = 'Spectral' })
-                        G.GAME.consumeable_buffer = 0
-                        return true
-                    end
+        if context.remove_playing_cards and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+            local contains = false
+            for _, c in pairs(context.removed) do
+                if c == card then
+                    contains = true
+                    break
                 end
-            }))
-            return { message = localize('k_plus_spectral'), colour = G.C.SECONDARY_SET.Spectral }
+            end
+            if contains then
+                G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + math.min(self.config.extra.cards, G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer))
+                for i = 1, math.min(card.ability.extra.tarots, G.consumeables.config.card_limit - #G.consumeables.cards) do
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'before',
+                        delay = 0.0,
+                        func = function()
+                                SMODS.add_card({ set = 'Spectral' })
+                                G.GAME.consumeable_buffer = 0
+                                return true
+                        end
+                    }))
+                    return { message = localize('k_plus_spectral'), colour = G.C.SECONDARY_SET.Spectral }
+                end
+            end
         end
     end
 })
+
+local can_calc_ref = Card.can_calculate
+function Card:can_calculate(ignore_debuff, ignore_sliced)
+    if self.seal == "hnds_spectralseal" then ignore_debuff = true end
+    return can_calc_ref(self, ignore_debuff, ignore_sliced)
+end
