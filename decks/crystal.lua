5b183d480c164fbeff18f5e1b6ba3d9e73770e27
Eris (borb) <remcosveen@gmail.com>
2025-10-10 17:06:46 +0200

    crystal deck half implemented

---
+++ decks/crystal.lua
@@ -0,0 +1,9 @@
+SMODS.Back {
+    key = "crystal",
+    atlas = "extras",
+    pos = { x = 1, y = 0 },
+    unlocked = true,
+    apply = function (self, back)
+        G.GAME.modifiers.hnds_double_showdown = true
+    end
+}
