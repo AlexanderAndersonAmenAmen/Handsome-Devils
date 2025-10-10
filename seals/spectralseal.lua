0fefea21574a437c1f75673c1a4d0ec7e1a645a1
Eris (borb) <remcosveen@gmail.com>
2025-10-10 13:40:19 +0200

    spectral seal fix attempt again

--- seals/spectralseal.lua
+++ seals/spectralseal.lua
@@ -4,4 +4,19 @@ SMODS.Seal({
     atlas = "Extras",
     badge_colour = G.C.SECONDARY_SET.Spectral,
     config = { extra = { cards = 2 } },
+    calculate = function (self, card, context)
+        if context.remove_playing_cards then
+            for _, c in ipairs(context.removed) do
+                if c == card then
+                    for _ = 1, math.min(self.config.extra.cards, G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer)) do
+                        G.E_MANAGER:add_event(Event({
+                            func = function ()
+                                SMODS.add_card({ set = "Spectral"})
+                            end
+                        }))
+                    end
+                end
+            end
+        end
+    end
 })
