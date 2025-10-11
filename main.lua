0efcc3674213384494178323e701b30e29dccef2
Eris (borb) <remcosveen@gmail.com>
2025-10-11 17:01:20 +0200

    change how art in packs works

--- main.lua
+++ main.lua
@@ -113,12 +113,15 @@ SMODS.current_mod.calculate = function(self, context)
 		}))
 		G.GAME.hnds_crystal_queued = nil
 	end
-	if context.setting_ability and G.pack_cards and context.other_card == G.pack_cards.cards[1] and not context.old then
-		context.other_card:set_ability("j_hnds_art")
-		for _, sticker in pairs(SMODS.Stickers) do
-			sticker:apply(context.other_card, nil)
-		end
-		context.other_card:set_edition()
+	if context.open_booster and G.GAME.art_queue > 0 then
+		G.E_MANAGER:add_event(Event({
+			func = function ()
+				SMODS.add_card({
+					area = G.pack_cards,
+					key = "j_hnds_art"
+				})
+			end
+		}))
 	end
 end
 
