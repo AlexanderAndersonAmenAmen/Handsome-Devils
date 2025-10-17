0347fa116e15a3e43828ffb41f09c5e1d66ca616
Eris (borb) <remcosveen@gmail.com>
2025-10-17 21:04:55 +0200

    fix nuisance

--- jokers/public_nuisance.lua
+++ jokers/public_nuisance.lua
@@ -14,7 +14,7 @@ SMODS.Joker({
 		return { vars = {} }
 	end,
 	remove_from_deck = function (self, card, from_debuff)
-		if G.GAME.chips >= G.GAME.blind.chips then
+		if G.GAME.chips >= G.GAME.blind.chips and G.GAME.blind.in_blind then
 			G.STATE = G.STATES.HAND_PLAYED
 	        G.STATE_COMPLETE = true
 	        end_round()
