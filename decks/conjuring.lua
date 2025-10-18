185b27747f03018cfffb2c8b826f76c567a08921
Eris (borb) <remcosveen@gmail.com>
2025-10-19 00:09:16 +0200

    kill

--- decks/conjuring.lua
+++ decks/conjuring.lua
@@ -13,7 +13,6 @@ SMODS.Back({
     pos = { x = 0, y = 2 },
     atlas = "Extras",
     apply = function(self,card)
-        G.GAME.modifiers.booster_choice_mod = (G.GAME.modifiers.booster_choice_mod or 0) + 1
         for _, booster in pairs(G.P_CENTER_POOLS.Booster) do
             if not string.find(booster.key:lower(), "mega") then
                 G.GAME.banned_keys[booster.key] = true
