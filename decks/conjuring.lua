947f987788e6eb07dacd2c5e46f8b60ea796cf57
Eris (borb) <remcosveen@gmail.com>
2025-10-18 23:28:01 +0200

    ban all non-mega boosters

--- decks/conjuring.lua
+++ decks/conjuring.lua
@@ -11,8 +11,11 @@ SMODS.Back ({
     pos = { x = 0, y = 2 },
     atlas = "Extras",
     apply = function(self,card)
-         G.GAME.modifiers.booster_choice_mod = (G.GAME.modifiers.booster_choice_mod or 0) + 1
+        G.GAME.modifiers.booster_choice_mod = (G.GAME.modifiers.booster_choice_mod or 0) + 1
+        for _, booster in pairs(G.P_CENTER_POOLS.Booster) do
+            if not string.find(booster.key:lower(), "mega") then
+                G.GAME.banned_keys[booster.key] = true
+            end
+        end
     end,
-    calculate = function(self, back, context)
-    end
 })
