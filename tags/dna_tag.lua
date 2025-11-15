883aeafb574ebef97312543cea034b27d62b17cb
Eris (borb) <remcosveen@gmail.com>
2025-11-15 15:02:19 +0100

    revert dna change because jank

--- tags/dna_tag.lua
+++ tags/dna_tag.lua
@@ -6,15 +6,13 @@ SMODS.Tag {
     discovered = true,
     apply = function(self, tag, context)
         if context.type == 'store_joker_modify' and context.card.ability.set == "Joker" then
-            local copy = copy_card(context.card)
-            create_shop_card_ui(copy, "Joker", G.shop_jokers)
-            copy.states.visible = false
-            copy.states.drag.can = false
+            local lock = tag.ID
+            G.CONTROLLER.locks[lock] = true
             tag:yep('+', G.C.GOLD, function()
-                copy:start_materialize()
-                G.shop_jokers:align_cards()
-                copy.ability.couponed = true
-                copy:set_cost()
+                context.card.ability.hnds_copies_to_create = (context.card.ability.hnds_copies_to_create or 0) + 1
+                context.card.ability.couponed = true
+                context.card:set_cost()
+                G.CONTROLLER.locks[lock] = nil
                 return true
             end)
             tag.triggered = true
