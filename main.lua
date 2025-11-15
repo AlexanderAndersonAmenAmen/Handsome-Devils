c06fb7c330cde529559a212ced64dd29d8aa3a0d
Eris (borb) <remcosveen@gmail.com>
2025-11-15 20:17:43 +0100

    oops

--- main.lua
+++ main.lua
@@ -170,21 +170,6 @@ SMODS.current_mod.config_tab = function()
 end
 
 SMODS.current_mod.calculate = function(self, context)
-	if context.drawing_cards and #G.GAME.green_seal_draws > 0 then --green seal effect
-		G.E_MANAGER:add_event(Event({
-			blockable = true,
-			func = function()
-				for _, card in ipairs(G.GAME.green_seal_draws) do
-					if card.area == G.deck and card.config then
-						draw_card(G.deck, G.hand, nil, "up", true, card)
-					end
-				end
-				G.GAME.green_seal_draws = {}
-				save_run()
-				return true
-			end
-		}))
-	end
 	if context.individual and SMODS.has_enhancement(context.other_card, "m_stone") then
 		G.GAME.ante_stones_scored = G.GAME.ante_stones_scored + 1
 	end
