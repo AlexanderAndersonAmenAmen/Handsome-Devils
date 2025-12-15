52637d3d1a55d34a9a36618d3ac865441bc86ce0
Eris (borb) <remcosveen@gmail.com>
2025-12-15 19:08:21 +0100

    try another fix

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -1,6 +1,6 @@
---[[  black seal and such card destruction hook (should no longer be needed but commented out just in case)
+--  black seal and such card destruction hook (should no longer be needed but commented out just in case)
 HNDS.should_hand_destroy = function(card)
-	return card.seal == "hnds_black" or (G.GAME.used_vouchers.v_hnds_soaked and card == G.hand.cards[1]) or
+	return card:get_seal() == "hnds_black" or (G.GAME.used_vouchers.v_hnds_soaked and card == G.hand.cards[1]) or
 	(G.GAME.used_vouchers.v_hnds_beyond and card == G.hand.cards[#G.hand.cards])
 end
 
@@ -33,7 +33,7 @@ function SMODS.calculate_destroying_cards(context, cards_destroyed, scoring_hand
 		end
 	end
 end
-]]
+
 local get_new_boss_ref = get_new_boss --crystal deck double showdown hook
 function get_new_boss()
 	local win_ante = G.GAME.win_ante
@@ -100,7 +100,7 @@ end
 
 score_card_ref = SMODS.score_card
 function SMODS.score_card(card, context)
-	if not G.scorehand and (card:get_seal()=="hnds_black" or (G.GAME.used_vouchers.v_hnds_soaked and card == G.hand.cards[1]) or (G.GAME.used_vouchers.v_hnds_beyond and card == G.hand.cards[#G.hand.cards])) and context.cardarea == G.hand then
+	if not G.scorehand and HNDS.should_hand_destroy(card) and context.cardarea == G.hand then
 		G.scorehand = true
 		context.cardarea = G.play
 		if context.destroy_card then context.destroying_card = context.destroy_card end
