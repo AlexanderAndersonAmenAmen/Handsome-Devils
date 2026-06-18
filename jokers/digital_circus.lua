78d2679da1a5986ac47fde974495462af428c491
Marffe <efframlol@gmail.com>
2026-06-18 16:59:44 -0500

    Simplyfied code for digital circus

--- jokers/digital_circus.lua
+++ jokers/digital_circus.lua
@@ -1,24 +1,18 @@
+local RARITY_NAMES = { 'k_common', 'k_uncommon', 'k_rare' }
+local RARITY_KEYS = { 'Common', 'Uncommon', 'Rare' }
+
 SMODS.Joker({
 	key = "digital_circus",
-	config = {
-		extra = {
-			max_rounds = 2,
-			current_rounds = 0,
-			current_rarity = 1,
-			rarity_strings = { 'k_common', 'k_uncommon', 'k_rare' },
-		},
-	},
+	config = { extra = { max_rounds = 2, current_rounds = 0, current_rarity = 1 } },
 	rarity = 3,
 	loc_vars = function(self, info_queue, card)
-		local rarity_key = card.ability.extra.rarity_strings[card.ability.extra.current_rarity]
+		local r = math.min(3, card.ability.extra.current_rarity)
 		return {
 			vars = {
-				localize(rarity_key),
+				localize(RARITY_NAMES[r]),
 				card.ability.extra.current_rounds,
 				card.ability.extra.max_rounds,
-				colours = {
-					G.C.RARITY[card.ability.extra.current_rarity],
-				},
+				colours = { G.C.RARITY[r] },
 			},
 		}
 	end,
@@ -32,45 +26,26 @@ SMODS.Joker({
 	eternal_compat = false,
 	perishable_compat = true,
 	calculate = function(self, card, context)
-		if context.selling_self or context.forcetrigger and not context.blueprint then
-			if #G.jokers.cards <= G.jokers.config.card_limit then
-				local rarity_vals = { "Common", "Uncommon", "Rare" }
-				if card.ability.extra.current_rarity > 3 then card.ability.extra.current_rarity = 3 end
-				SMODS.add_card({
-					set = "Joker",
-					area = G.jokers,
-					rarity = rarity_vals[card.ability.extra.current_rarity],
-					edition = poll_edition("digital_circus", 1, false, true),
-				})
-				return nil, true
-			else
+		if (context.selling_self or context.forcetrigger) and not context.blueprint then
+			if #G.jokers.cards > G.jokers.config.card_limit then
 				return { message = localize("k_no_room_ex") }
 			end
+			local r = math.min(3, card.ability.extra.current_rarity)
+			SMODS.add_card({ set = "Joker", area = G.jokers, rarity = RARITY_KEYS[r], edition = poll_edition("digital_circus", 1, false, true) })
+			return nil, true
 		end
 
 		if (context.end_of_round and context.main_eval) or context.forcetrigger then
-			if card.ability.extra.current_rarity > 3 then card.ability.extra.current_rarity = 3 end
-			if card.ability.extra.current_rarity < 3 then
-				card.ability.extra.current_rounds = card.ability.extra.current_rounds + 1
-				if card.ability.extra.current_rounds >= card.ability.extra.max_rounds then
-					card.ability.extra.current_rarity = math.min(3, card.ability.extra.current_rarity + 1)
-					card.ability.extra.current_rounds = 0
-				end
-				if
-					card.ability.extra.current_rounds ~= 0
-					and card.ability.extra.current_rounds ~= card.ability.extra.max_rounds
-				then
-					return {
-						message = card.ability.extra.current_rounds .. "/" .. card.ability.extra.max_rounds,
-						colour = G.C.FILTER,
-					}
-				else
-					return {
-						message = localize(card.ability.extra.rarity_strings[card.ability.extra.current_rarity]) .. "!",
-						colour = G.C.RARITY[card.ability.extra.current_rarity],
-					}
-				end
+			local r = card.ability.extra.current_rarity
+			if r >= 3 then return end
+			card.ability.extra.current_rounds = card.ability.extra.current_rounds + 1
+			if card.ability.extra.current_rounds < card.ability.extra.max_rounds then
+				return { message = card.ability.extra.current_rounds .. "/" .. card.ability.extra.max_rounds, colour = G.C.FILTER }
 			end
+			r = math.min(3, r + 1)
+			card.ability.extra.current_rarity = r
+			card.ability.extra.current_rounds = 0
+			return { message = localize(RARITY_NAMES[r]) .. "!", colour = G.C.RARITY[r] }
 		end
 	end,
 	joker_display_def = function(JokerDisplay)
@@ -78,22 +53,19 @@ SMODS.Joker({
             reminder_text = {
                 { text = "(" },
                 { ref_table = "card.joker_display_values", ref_value = "rarity" },
-                { text = ") " },
-                { text = "(" },
-                { ref_table = "card.joker_display_values", ref_value = "current_rounds" },
+                { text = ") (" },
+                { ref_table = "card.joker_display_values", ref_value = "rounds" },
                 { text = "/" },
-                { ref_table = "card.joker_display_values", ref_value = "max_rounds" },
+                { ref_table = "card.joker_display_values", ref_value = "max" },
                 { text = ")" }
             },
             calc_function = function(card)
-                local r = card.ability.extra.current_rarity
-                if r > 3 then r = 3 end
-                local rarity_key = card.ability.extra.rarity_strings[r]
-                card.joker_display_values.rarity = localize(rarity_key)
-                card.joker_display_values.current_rounds = card.ability.extra.current_rounds
-                card.joker_display_values.max_rounds = card.ability.extra.max_rounds
+                local r = math.min(3, card.ability.extra.current_rarity)
+                card.joker_display_values.rarity = localize(RARITY_NAMES[r])
+                card.joker_display_values.rounds = card.ability.extra.current_rounds
+                card.joker_display_values.max = card.ability.extra.max_rounds
             end
         }
     end,
-	attributes = { "joker" }
+	attributes = { "joker", "scaling", "generation" }
 })
