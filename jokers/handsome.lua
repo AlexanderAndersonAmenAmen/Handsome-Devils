b1c819922965420bee66c382f6c687c8661cca2f
Marffe <efframlol@gmail.com>
2026-06-24 02:12:06 -0500

    Handsome Devil only appears if you have a Joker or Card with an edition

--- jokers/handsome.lua
+++ jokers/handsome.lua
@@ -8,6 +8,15 @@ SMODS.Joker {
     discovered = true,
     blueprint_compat = true,
     perishable_compat = true,
+    in_pool = function(self, args)
+        for _, c in ipairs(G.jokers.cards) do
+            if c.edition and c.edition.negative == nil and next(c.edition) then return true end
+        end
+        for _, c in ipairs(G.playing_cards) do
+            if c.edition and c.edition.negative == nil and next(c.edition) then return true end
+        end
+        return false
+    end,
     calculate = function (self, card, context)
         if (context.repetition or (context.retrigger_joker_check and not context.retrigger_joker)) and context.other_card.edition and context.other_card ~= card then --should this also retrigger jokers
             return { repetitions = 1 }
