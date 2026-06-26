869ec754982d54c87e127dc62a495f650b354b1a
Marffe <efframlol@gmail.com>
2026-06-26 02:39:33 -0500

    Premium deck had an unused text

--- decks/premiumdeck.lua
+++ decks/premiumdeck.lua
@@ -4,10 +4,4 @@ SMODS.Back({
 	atlas = "Extras",
 	config = { vouchers = { 'v_hnds_premium', 'v_hnds_top_shelf' } },
 	unlocked = true,
-	loc_vars = function(self, info_queue, back)
-		return {
-			vars = { localize { type = 'name_text', key = self.config.voucher, set = 'Voucher' }
-			}
-		}
-	end,
 })
