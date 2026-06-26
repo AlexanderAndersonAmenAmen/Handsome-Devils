61eb1f10d2eceb65f2e7889e4e4468d85ffa3ad6
Marffe <efframlol@gmail.com>
2026-06-26 02:34:52 -0500

    Conjuring deck, simplyfied the booster pack logic

--- decks/conjuring.lua
+++ decks/conjuring.lua
@@ -7,11 +7,7 @@ SMODS.Back({
     pos = { x = 0, y = 2 },
     atlas = "Extras",
     apply = function(self,card)
-        for _, booster in pairs(G.P_CENTER_POOLS.Booster) do
-            if booster.kind ~= "hnds_magic" then
-                G.GAME.banned_keys[booster.key] = true
-            end
-        end
+        HNDS.ban_non_magic_boosters()
     end,
     pools = { RedeemableBacks = true }
 })
@@ -32,343 +28,67 @@ local magic_diha_compat = { --cryptid digital hallucinations compat
     end
 }
 
-SMODS.Booster {
-    key = "magic_1",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 4, y = 0 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
+-- The six magic packs are identical except for their atlas sprite position.
+local magic_pack_positions = {
+    { x = 4, y = 0 }, { x = 5, y = 0 },
+    { x = 4, y = 1 }, { x = 5, y = 1 },
+    { x = 4, y = 2 }, { x = 5, y = 2 },
 }
 
-
-SMODS.Booster {
-    key = "magic_2",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 5, y = 0 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
-}
-
-
-SMODS.Booster {
-    key = "magic_3",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 4, y = 1 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
-}
-
-
-SMODS.Booster {
-    key = "magic_4",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 5, y = 1 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
-}
-
-
-SMODS.Booster {
-    key = "magic_5",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 4, y = 2 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
-}
-
-
-SMODS.Booster {
-    key = "magic_6",
-    weight = 0.25,
-    kind = "hnds_magic",
-    cost = 10,
-    atlas = "Extras",
-    pos = { x = 5, y = 2 },
-    config = { extra = 5, choose = 2 },
-    draw_hand = true,
-    group_key = "k_hnds_magic_pack",
-    loc_vars = function (self, info_queue, card)
-        local cfg = (card and card.ability) or self.config or {}
-        return {
-            vars = { cfg.extra, cfg.choose },
-            key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
-        }
-    end,
-    ease_background_colour = function (self) --colours in the pack, subject to change probably
-        ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
-        ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
-    end,
-    particles = function (self)
-        G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
-            timer = 0.015,
-            scale = 0.2,
-            initialize = true,
-            lifespan = 1,
-            speed = 1.1,
-            padding = -1,
-            attach = G.ROOM_ATTACH,
-            colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
-            fill = true
-        })
-        G.booster_pack_sparkles.fade_alpha = 1
-        G.booster_pack_sparkles:fade(1, 0)
-    end,
-    create_card = function (self, card, i)
-        local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
-        return {
-            set = pool,
-            area = G.pack_cards,
-            skip_materialize = true,
-            soulable = true,
-            key_append = "hnds_magic_card",
-            seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
-            edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
-        }
-    end,
-    cry_digital_hallucinations = magic_diha_compat,
-    in_pool = function (self, args)
-        local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
-        local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
-        return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
-    end
-}
+for pack_index, pack_pos in ipairs(magic_pack_positions) do
+    SMODS.Booster {
+        key = "magic_" .. pack_index,
+        weight = 0.25,
+        kind = "hnds_magic",
+        cost = 10,
+        atlas = "Extras",
+        pos = pack_pos,
+        config = { extra = 5, choose = 2 },
+        draw_hand = true,
+        group_key = "k_hnds_magic_pack",
+        loc_vars = function (self, info_queue, card)
+            local cfg = (card and card.ability) or self.config or {}
+            return {
+                vars = { cfg.extra, cfg.choose },
+                key = self.key:sub(1, -3) --gets rid of the number at the end for the description key
+            }
+        end,
+        ease_background_colour = function (self) --colours in the pack, subject to change probably
+            ease_colour(G.C.DYN_UI.MAIN, G.C.DARK_EDITION)
+            ease_background_colour({ new_colour = G.C.FILTER, special_colour = G.C.BLACK, contrast = 2 })
+        end,
+        particles = function (self)
+            G.booster_pack_sparkles = Particles(1, 1, 0, 0, { --this is just arcana pack particle code, feel free to change some values
+                timer = 0.015,
+                scale = 0.2,
+                initialize = true,
+                lifespan = 1,
+                speed = 1.1,
+                padding = -1,
+                attach = G.ROOM_ATTACH,
+                colours = { G.C.BLACK, G.C.RED, G.C.SECONDARY_SET.Joker, G.C.SECONDARY_SET.Planet, G.C.SECONDARY_SET.Tarot, G.C.SECONDARY_SET.Spectral }, --im just messing around with colours tbh
+                fill = true
+            })
+            G.booster_pack_sparkles.fade_alpha = 1
+            G.booster_pack_sparkles:fade(1, 0)
+        end,
+        create_card = function (self, card, i)
+            local pool = pseudorandom_element({"Joker", "Playing Card", "Consumeables"}, "hnds_magic_set")
+            return {
+                set = pool,
+                area = G.pack_cards,
+                skip_materialize = true,
+                soulable = true,
+                key_append = "hnds_magic_card",
+                seal = pool == "Playing Card" and SMODS.poll_seal({ key = "magic_seal" }),
+                edition = pool == "Playing Card" and SMODS.poll_edition({ key = "magic_edition" })
+            }
+        end,
+        cry_digital_hallucinations = magic_diha_compat,
+        in_pool = function (self, args)
+            local hasConjuringDeck = G.GAME.selected_back and G.GAME.selected_back.effect.center.key == "b_hnds_conjuring"
+            local hasConjuringSleeve = CardSleeves and G.GAME.selected_sleeve == "sleeve_hnds_conjuring_sleeve"
+            return hasConjuringDeck or hasConjuringSleeve or hnds_config.enableMagicPackSpawning
+        end
+    }
+end
