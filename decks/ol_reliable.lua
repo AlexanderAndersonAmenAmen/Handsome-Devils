9a177f2425c60d7c4f34e403a68dd96a9f36120d
Marffe <efframlol@gmail.com>
2026-06-26 02:39:06 -0500

    Simplyfied the logic of the Lucky deck xd

--- decks/ol_reliable.lua
+++ decks/ol_reliable.lua
@@ -4,13 +4,11 @@ SMODS.Back {
     pos = { x = 1, y = 2 },
     unlocked = true,
     calculate = function(self, back, context)
-         if context.mod_probability and not context.blueprint and (G.shop or (G.GAME.blind and G.GAME.blind.boss)) then
-            if G.GAME and G.GAME.selected_sleeve == "sleeve_hnds_ol_sleeve" then
-            else
-                return {
-                    numerator = context.numerator * 3
-                }
-            end
+        if context.mod_probability and not context.blueprint and (G.shop or (G.GAME.blind and G.GAME.blind.boss))
+            and not (G.GAME and G.GAME.selected_sleeve == "sleeve_hnds_ol_sleeve") then
+            return {
+                numerator = context.numerator * 3
+            }
         end
     end,
     pools = { RedeemableBacks = true }
