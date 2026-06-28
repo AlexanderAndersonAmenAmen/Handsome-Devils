8438f207bd7bc6551d962746258ddd1caaeccf1e
Marffe <efframlol@gmail.com>
2026-06-28 15:35:09 -0500

    Public Nuisance: Looks like the new SMODS version handles this problems better now so we dont need the code

--- jokers/public_nuisance.lua
+++ jokers/public_nuisance.lua
@@ -10,29 +10,10 @@ SMODS.Joker({
 	demicoloncompat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	loc_vars = function(self, info_queue, card)
-		return {}
-	end,
 	remove_from_deck = function(self, card, from_debuff)
 		if not (G.GAME.blind and G.GAME.blind.in_blind) then return end
 		if G.GAME.chips >= G.GAME.blind.chips then
-			if G.deck and G.deck.cards and #G.deck.cards > 0 then
-				G.STATE = G.STATES.HAND_PLAYED
-				G.STATE_COMPLETE = true
 				end_round()
-			else
-				-- Player has no cards left; check if chips were scored
-				if G.GAME.chips >= G.GAME.blind.chips then
-					G.GAME.blind.chips = 0
-					G.GAME.blind.disabled = true
-					G.STATE = G.STATES.POST_BLIND
-					G.STATE_COMPLETE = true
-				else
-					-- The player is aLoser LMAO, if you ask yourself if this is necessary I got into a situation where I got rid of all my cards and still having 8 hands, then I got the Mr. Bones glitch
-					G.STATE = G.STATES.GAME_OVER
-					G.STATE_COMPLETE = true
-				end
-			end
 		end
 	end,
 	attributes = { "passive", "hands" }
